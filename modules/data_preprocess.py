3e74e0c8fcf1cd88a5a7f36a0a0cf8a7d1999c24
Barizki <nbrzk.gh@gmail.com>
2023-01-21 17:52:34 +0700

    Update File

--- modules/data_preprocess.py
+++ modules/data_preprocess.py
@@ -70,8 +70,9 @@ class LoanDataPreprocess(BaseEstimator, TransformerMixin):
         return X
 
 class LoanDataLabelPredictor(BaseEstimator, TransformerMixin):
-    def __init__(self, include_post_origin=[]):
-        self.include_post_origin = include_post_origin
+    def __init__(self, include=[], exclude=[]):
+        self.include = include
+        self.exclude = exclude
 
     def fit(self, X=None, y=None):
         self.applicant_features_ = [
@@ -110,19 +111,22 @@ class LoanDataLabelPredictor(BaseEstimator, TransformerMixin):
     
     def transform(self, X, y=None):
         # Filter to only have 'INDIVIDUAL'
-        X = X[X.application_type == 'INDIVIDUAL']
+        X = X[X.application_type.str.upper() == 'INDIVIDUAL']
         # Create Label
         X = X.assign(
             loan_category=X.loan_status.map(self.mapping_loan_cat_)
             )
         # Filter to only have Good Loan and Bad Loan
         X = X[X.loan_category.isin(['Good Loan', 'Bad Loan'])].reset_index()
-        label = X.loan_category.values
         # return only selected 
-        return (
-            X[self.applicant_features_ + self.loan_features_ + self.include_post_origin],
-            label
-            )
+        if not self.exclude:
+            return X[['loan_category']
+                     + self.applicant_features_ 
+                     + self.loan_features_ + self.include],
+        else:
+            return X[['loan_category']
+                     + self.applicant_features_ 
+                     + self.loan_features_ + self.include].drop(columns=self.exclude)
 
 class LoanDataMissingHandler(BaseEstimator, TransformerMixin):
     def __init__(self):
@@ -141,7 +145,7 @@ class LoanDataMissingHandler(BaseEstimator, TransformerMixin):
         return self 
 
     def transform(self, X, y=None):
-        self.X = X.copy()
+        self.X = X
         # mths_since_last_record
         rec_condition_1 = (self.X.mths_since_last_record.isna()) & (self.X.pub_rec == 0)
         self._perform(rec_condition_1, 'mths_since_last_record', 99)
@@ -320,8 +324,11 @@ class LoanDataMissingHandler(BaseEstimator, TransformerMixin):
             'grade', 'sub_grade', 'pymnt_plan'
             ]
         for feature in mandatory_features:
-            condition = self.X[feature].isna()
-            self._perform(condition, drop_obs=True)
+            try:
+                condition = self.X[feature].isna()
+                self._perform(condition, drop_obs=True)
+            except KeyError:
+                continue
         ## replace missing values outside special conditions
         # replace 0
         replace_0 = [
@@ -331,12 +338,237 @@ class LoanDataMissingHandler(BaseEstimator, TransformerMixin):
             'delinq_2yrs', 'max_bal_bc'
             ]
         for feature in replace_0:
-            self.X.loc[:, feature] = self.X.loc[:, feature].fillna(0)
+            try:
+                self.X.loc[:, feature] = self.X.loc[:, feature].fillna(0)
+            except KeyError:
+                continue
         replace_99 = [
             'mths_since_rcnt_il', 'mths_since_last_record', 'mths_since_last_major_derog', 'mths_since_last_delinq',
             ]
         for feature in replace_99:
-            self.X.loc[:, feature] = self.X.loc[:, feature].fillna(99)
-        if y:
-            return self.X.reset_index(drop=True), y[self.X.index]
-        return self.X.reset_index(drop=True)
+            try:
+                self.X.loc[:, feature] = self.X.loc[:, feature].fillna(99)
+            except KeyError:
+                continue
+        label = self.X.loan_category.values
+        predictor = self.X.drop(columns=['loan_category']).reset_index(drop=True)
+        return predictor, label
+
+# class LoanDataMissingHandler(BaseEstimator, TransformerMixin):
+#     def __init__(self):
+#         pass
+
+#     def _perform(self, masking, feature=None, replace_value=None, drop_obs=False):
+#         nonlocal X
+#         if replace_value:
+#             X.loc[masking, feature] = replace_value
+#         if drop_obs:
+#             X = X.drop(X[masking].index)
+
+#     def fit(self, X, y=None):
+#         self.median_cond_3_ = X.loc[
+#             (X.delinq_2yrs > 0) & (X.acc_now_delinq == 0),
+#             'mths_since_last_delinq'].median()
+#         return self 
+
+#     def transform(self, X, y=None):
+#         self.X = 
+#         # mths_since_last_record
+#         rec_condition_1 = (X.mths_since_last_record.isna()) & (X.pub_rec == 0)
+#         self._perform(rec_condition_1, 'mths_since_last_record', 99)
+#         rec_condition_2 = (X.mths_since_last_record.isna()) & (X.pub_rec > 0)
+#         self._perform(rec_condition_2, 'mths_since_last_record', 1)
+#         rec_condition_3 = (X.mths_since_last_record.isna()) & (X.pub_rec.isna())
+#         self._perform(rec_condition_3, drop_obs=True)
+#         # mths_since_last_delinq
+#         last_delinq_condition_1 = \
+#             (X.mths_since_last_delinq.isna())\
+#             & (X.delinq_2yrs == 0)\
+#             & (X.acc_now_delinq == 0)
+#         self._perform(last_delinq_condition_1, 'mths_since_last_delinq', 99)
+#         last_delinq_condition_2 = \
+#             (X.mths_since_last_delinq.isna())\
+#             & (X.delinq_2yrs > 0)\
+#             & (X.acc_now_delinq > 0)
+#         self._perform(last_delinq_condition_2, 'mths_since_last_delinq', 1)
+#         last_delinq_condition_3 = \
+#             (X.mths_since_last_delinq.isna())\
+#             & (X.delinq_2yrs > 0)\
+#             & (X.acc_now_delinq == 0)
+#         self._perform(last_delinq_condition_3, 'mths_since_last_delinq', self.median_cond_3_)
+#         last_delinq_condition_4 = \
+#             (X.mths_since_last_delinq == 0)\
+#             & (X.delinq_2yrs > 0)\
+#             & (X.acc_now_delinq == 0)
+#         self._perform(last_delinq_condition_4, 'mths_since_last_delinq', self.median_cond_3_)
+#         last_delinq_condition_5 = \
+#             (X.mths_since_last_delinq.between(0, 25, inclusive='neither'))\
+#             & (X.delinq_2yrs == 0)\
+#             & (X.acc_now_delinq == 0)
+#         self._perform(last_delinq_condition_5, 'delinq_2yrs', 1)
+#         last_delinq_condition_6 = \
+#             (X.mths_since_last_delinq == 0)\
+#             & (X.delinq_2yrs == 0)\
+#             & (X.acc_now_delinq == 0)
+#         self._perform(last_delinq_condition_6, 'mths_since_last_delinq', 99)
+#         last_delinq_condition_7 = \
+#             (X.mths_since_last_delinq.isna())\
+#             & (X.delinq_2yrs.isna())\
+#             & (X.acc_now_delinq.isna())
+#         self._perform(last_delinq_condition_7, drop_obs=True)
+#         # inq_last_{6mths, 12mths}
+#         inq_condition_1 = \
+#             (X.inq_last_6mths.isna() | X.inq_last_12m.isna())\
+#             & (X.inq_fi == 0)
+#         self._perform(inq_condition_1, ['inq_last_6mths', 'inq_last_12m'], 0)
+#         inq_condition_2 = \
+#             (X.inq_last_6mths.isna() | X.inq_last_12m.isna())\
+#             & (X.inq_fi > 0)
+#         try:
+#             self._perform(inq_condition_2, 'inq_last_6mths', X.loc[inq_condition_2, 'inq_fi'])
+#             self._perform(inq_condition_2, 'inq_last_12m', X.loc[inq_condition_2, 'inq_fi'])
+#         except ValueError:
+#             pass
+#         inq_condition_3 = \
+#             (X.inq_last_6mths.isna() & X.inq_last_12m.isna())\
+#             & (X.inq_fi.isna())
+#         self._perform(inq_condition_3, drop_obs=True)
+#         # open_acc
+#         acc_condition_1_2 = \
+#             (X.open_acc.isna() | X.total_acc.isna())\
+#             & ((X.open_acc == 0) | (X.total_acc == 0))
+#         self._perform(acc_condition_1_2, 'open_acc', 0)
+#         self._perform(acc_condition_1_2, 'total_acc', 0)
+#         acc_condition_3_4 = \
+#             (X.open_acc.isna() | X.total_acc.isna())\
+#             & ((X.open_acc > 0) | (X.total_acc > 0))
+#         self._perform(acc_condition_3_4, drop_obs=True)
+#         acc_condition_5 = \
+#             X.open_acc.isna() & X.total_acc.isna()
+#         self._perform(acc_condition_5, drop_obs=True)
+#         # total_acc, tot_cur_bar
+#         bal_condition_1_2 = \
+#             (X.tot_cur_bal.isna() | X.total_acc.isna())\
+#             & ((X.tot_cur_bal == 0) | (X.total_acc == 0))
+#         self._perform(bal_condition_1_2, 'total_acc', 0)
+#         self._perform(bal_condition_1_2, 'tot_cur_bal', 0)
+#         bal_condition_3_4 = \
+#             (X.tot_cur_bal.isna() | X.total_acc.isna())\
+#             & ((X.tot_cur_bal > 0) | (X.total_acc > 0))
+#         self._perform(bal_condition_3_4, drop_obs=True)
+#         bal_condition_5 = \
+#             X.tot_cur_bal.isna() & X.total_acc.isna()
+#         self._perform(bal_condition_5, drop_obs=True)
+#         # total_coll_amnt
+#         coll_condition_1 = \
+#             X.tot_coll_amt.isna() & X.collections_12_mths_ex_med.isna()
+#         self._perform(coll_condition_1, drop_obs=True)
+#         coll_condition_2_3 = \
+#             (X.tot_coll_amt.isna() | (X.tot_coll_amt == 0))\
+#             & (X.collections_12_mths_ex_med.isna() | (X.collections_12_mths_ex_med == 0))
+#         self._perform(coll_condition_2_3, 'tot_coll_amt', 0)
+#         self._perform(coll_condition_2_3, 'collections_12_mths_ex_med', 0)
+#         coll_condition_4 = \
+#             X.tot_coll_amt.isna() & (X.collections_12_mths_ex_med > 0)
+#         self._perform(coll_condition_4, drop_obs=True)
+#         coll_condition_5 = \
+#             (X.tot_coll_amt == 0) & (X.collections_12_mths_ex_med.isna())
+#         self._perform(coll_condition_5, drop_obs=True)
+#         # open_il
+#         open_il_condition_1 = \
+#             (X.open_il_12m.isna() | X.open_il_24m.isna())\
+#             & (X.mths_since_rcnt_il.isna())\
+#             & (X.total_bal_il == 0)
+#         self._perform(open_il_condition_1, 'mths_since_rcnt_il', 99)
+#         self._perform(open_il_condition_1, 'open_il_12m', 0)
+#         self._perform(open_il_condition_1, 'open_il_24m', 0)
+#         open_il_condition_2 = \
+#             (X.open_il_12m.isna() | X.open_il_24m.isna())\
+#             & (X.mths_since_rcnt_il.isna())\
+#             & (X.total_bal_il > 0)
+#         self._perform(open_il_condition_2, drop_obs=True)
+#         open_il_condition_3 = \
+#             ((X.open_il_12m > 0) | (X.open_il_24m > 0))\
+#             & (X.mths_since_rcnt_il.isna())\
+#             & (X.total_bal_il > 0)
+#         self._perform(open_il_condition_3, 'mths_since_rcnt_il', 1)
+#         open_il_condition_4 = \
+#             (X.open_il_12m.isna() & X.open_il_24m.isna())\
+#             & (X.mths_since_rcnt_il.isna())\
+#             & (X.total_bal_il.isna())
+#         self._perform(open_il_condition_4, 'open_il_12m', 0) 
+#         self._perform(open_il_condition_4, 'open_il_24m', 0) 
+#         self._perform(open_il_condition_4, 'mths_since_rcnt_il', 99) 
+#         self._perform(open_il_condition_4, 'total_bal_il', 0)
+#         # total_bal_il
+#         bal_il_condition_1 = \
+#             (X.total_bal_il > 0)\
+#             & (X.il_util.isna())
+#         self._perform(bal_il_condition_1, drop_obs=True)
+#         bal_il_condition_2 = \
+#             (X.total_bal_il.isna())\
+#             & (X.il_util > 0)
+#         self._perform(bal_il_condition_2, drop_obs=True)
+#         bal_il_condition_3 = \
+#             (X.total_bal_il.isna())\
+#             & (X.il_util == 0)
+#         self._perform(bal_il_condition_3, 'total_bal_il', 0)
+#         bal_il_condition_4 = \
+#             (X.total_bal_il == 0)\
+#             & (X.il_util.isna())
+#         self._perform(bal_il_condition_4, 'il_util', 0)
+#         bal_il_condition_5 = \
+#             (X.total_bal_il.isna())\
+#             & (X.il_util.isna())
+#         self._perform(bal_il_condition_5, 'total_bal_il', 0)
+#         self._perform(bal_il_condition_5, 'il_util', 0)
+#         # revol_bal
+#         revol_condition_1 = \
+#             (X.revol_bal > 0)\
+#             & (X.revol_util.isna())
+#         self._perform(revol_condition_1, drop_obs=True)
+#         revol_condition_2 = \
+#             (X.revol_bal.isna())\
+#             & (X.revol_util > 0)
+#         self._perform(revol_condition_2, drop_obs=True)
+#         revol_condition_3 = \
+#             (X.revol_bal.isna())\
+#             & (X.revol_util == 0)
+#         self._perform(revol_condition_3, 'revol_bal', 0)
+#         revol_condition_4 = \
+#             (X.revol_bal == 0)\
+#             & (X.revol_util.isna())
+#         self._perform(revol_condition_4, 'revol_util', 0)
+#         revol_condition_5 = \
+#             (X.revol_bal.isna())\
+#             & (X.revol_util.isna())
+#         self._perform(revol_condition_5, 'revol_bal', 0)
+#         self._perform(revol_condition_5, 'revol_util', 0)
+#         ## Mandatory Features
+#         mandatory_features = [
+#             'annual_inc', 'dti', 'home_ownership',
+#             'loan_amnt', 'term', 'int_rate', 'installment', 
+#             'grade', 'sub_grade', 'pymnt_plan'
+#             ]
+#         for feature in mandatory_features:
+#             condition = X[feature].isna()
+#             self._perform(condition, drop_obs=True)
+#         ## replace missing values outside special conditions
+#         # replace 0
+#         replace_0 = [
+#             'il_util', 'total_cu_tl', 'inq_last_12m', 'all_util', 'open_rv_24m', 'open_rv_12m', 'open_acc_6m', 
+#             'total_bal_il', 'inq_fi', 'open_il_12m', 'open_il_24m', 'tot_coll_amt', 'tot_cur_bal', 'total_rev_hi_lim',
+#             'revol_util', 'collections_12_mths_ex_med', 'pub_rec', 'acc_now_delinq', 'total_acc', 'open_acc', 'inq_last_6mths',
+#             'delinq_2yrs', 'max_bal_bc'
+#             ]
+#         for feature in replace_0:
+#             X.loc[:, feature] = X.loc[:, feature].fillna(0)
+#         # replace 99
+#         replace_99 = [
+#             'mths_since_rcnt_il', 'mths_since_last_record', 'mths_since_last_major_derog', 'mths_since_last_delinq',
+#             ]
+#         for feature in replace_99:
+#             X.loc[:, feature] = X.loc[:, feature].fillna(99)
+#         label = X.loan_category.values
+#         predictor = X.drop(columns=['loan_category']).reset_index(drop=True)
+#         return predictor, label
